2120095971011b8a5a107cd01b52846d52fa194c
Vishal2690 <vishalc2690@gmail.com>
2023-04-11 18:12:15 +0530

    first c

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PureCompo from './Five Handson/Pure Compo/PureCompo'
+import ChildCompo from './Five Handson/HOC/ChildCompo'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+    
+    <ChildCompo/>
+    <PureCompo/>
+      
+    
+    </div>
+  )
+}
+
+export default App
